47667f8ede8cfd4f0bc3e9c236f3679d8cb5362c
Ryan Hodges <ryan.d.hodges@gmail.com>
2020-03-23 10:38:48 -0700

    moving testing default data into testing dir, cleaning template dir

--- testing/test_INPUT.py
+++ testing/test_INPUT.py
@@ -5,11 +5,11 @@ def test_input_generation():
     import filecmp
     filecmp.clear_cache()
     workspace = '/usr/local/apps/workspace'
-    BASIN_INPUT_FILE = os.path.join(workspace, 'testing/test_basin/INPUT.wenatchee.testing')
-    OUTPUT_FILE = os.path.join(workspace, 'templates/basin/scenarios/scenario/run/INPUT.basin.scenario')
+    BASIN_INPUT_FILE = os.path.join(workspace, 'testing/basin/INPUT.wenatchee.testing')
+    OUTPUT_FILE = os.path.join(workspace, 'testing/basin/scenarios/scenario/run/INPUT.basin.scenario')
     python_script = os.path.join(workspace, 'run_scenario.py')
     # Run default template creation
-    os.system('%s %s -p %s/templates/ -b basin -s scenario' % (sys.executable, python_script, workspace))
+    os.system('%s %s -p %s/testing/ -b basin -s scenario' % (sys.executable, python_script, workspace))
     # Compare output to existing default basin input file
     # filecmp.cmp does not seem to have an 'ignore whitespace' option
     #       Using *nix diff instead (-b for 'ignore whitespace')
